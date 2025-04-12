91535741f39ef21c9c99b0e36e6a1c7a1f4d4ffc
ahmedfathallah2 <ahmedfathallah202@gmail.com>
2025-04-12 17:08:27 +0200

    delet

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,7 +1,6 @@
 import 'package:ecommerce_app/chat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'chatscreen.dart';
 import 'profile.dart';
 import 'productdetails.dart';
 import 'models/product.dart';
